a044a42bf81922c1d400a5e44c8038f73f03aaa0
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:39:51 +0200

    'Update'

--- StatusLEDs_InspectorP/scripts/StatusLEDs.lua
+++ StatusLEDs_InspectorP/scripts/StatusLEDs.lua
@@ -1,20 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  StatusLEDs_InspectorP
-
-  Summary:
-  Alternating the result LED between different colors.
-
-  How to Run:
-  A connected InpspectorP device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10).
-  The 'Result' LED cycles through the available colors.
-
-  More Information:
-  See the tutorial "Audio-visual Feedback InspectorP".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 --Array of available colors
